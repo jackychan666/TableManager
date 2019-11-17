574385e2e093879298447b8d1fd1b74924129a21
jackychan666 <jackychan1996@outlook.com>
2019-11-17 15:31:29 +0800

    add a project

---
+++ TableManager/TableManager/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace TableManager
+{
+    partial class Win_Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CB_ChooseTab = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.DTP_beginTime = new System.Windows.Forms.DateTimePicker();
+            this.DTP_endTime = new System.Windows.Forms.DateTimePicker();
+            this.L_timeBegin = new System.Windows.Forms.Label();
+            this.L_timeEnd = new System.Windows.Forms.Label();
+            this.B_Confirm = new System.Windows.Forms.Button();
+            this.B_Exit = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.B_ViewData = new System.Windows.Forms.Button();
+            this.B_DeletSql = new System.Windows.Forms.Button();
+            this.TB_equipName = new System.Windows.Forms.TextBox();
+            this.L_equipName = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // CB_ChooseTab
+            // 
+            this.CB_ChooseTab.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.CB_ChooseTab.FormattingEnabled = true;
+            this.CB_ChooseTab.Items.AddRange(new object[] {
+            "模式接收记录",
+            "模式运行记录",
+            "报警记录",
+            "模拟量历史记录",
+            "用户操作记录",
+            "设备动作记录"});
+            this.CB_ChooseTab.Location = new System.Drawing.Point(165, 14);
+            this.CB_ChooseTab.Name = "CB_ChooseTab";
+            this.CB_ChooseTab.Size = new System.Drawing.Size(121, 20);
+            this.CB_ChooseTab.TabIndex = 0;
+            this.CB_ChooseTab.SelectedIndexChanged += new System.EventHandler(this.Choose_mod_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "选择维修记录表";
+            // 
+            // DTP_beginTime
+            // 
+            this.DTP_beginTime.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.DTP_beginTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.DTP_beginTime.Location = new System.Drawing.Point(165, 86);
+            this.DTP_beginTime.MaxDate = new System.DateTime(2030, 12, 31, 0, 0, 0, 0);
+            this.DTP_beginTime.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.DTP_beginTime.Name = "DTP_beginTime";
+            this.DTP_beginTime.Size = new System.Drawing.Size(200, 21);
+            this.DTP_beginTime.TabIndex = 2;
+            // 
+            // DTP_endTime
+            // 
+            this.DTP_endTime.CustomFormat = "yyyy-MM-dd HH:mm:ss";
+            this.DTP_endTime.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.DTP_endTime.Location = new System.Drawing.Point(165, 123);
+            this.DTP_endTime.MaxDate = new System.DateTime(2030, 12, 31, 0, 0, 0, 0);
+            this.DTP_endTime.MinDate = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            this.DTP_endTime.Name = "DTP_endTime";
+            this.DTP_endTime.Size = new System.Drawing.Size(200, 21);
+            this.DTP_endTime.TabIndex = 3;
+            // 
+            // L_timeBegin
+            // 
+            this.L_timeBegin.AutoSize = true;
+            this.L_timeBegin.Location = new System.Drawing.Point(67, 94);
+            this.L_timeBegin.Name = "L_timeBegin";
+            this.L_timeBegin.Size = new System.Drawing.Size(89, 12);
+            this.L_timeBegin.TabIndex = 4;
+            this.L_timeBegin.Text = "记录表开始时间";
+            // 
+            // L_timeEnd
+            // 
+            this.L_timeEnd.AutoSize = true;
+            this.L_timeEnd.Location = new System.Drawing.Point(67, 127);
+            this.L_timeEnd.Name = "L_timeEnd";
+            this.L_timeEnd.Size = new System.Drawing.Size(89, 12);
+            this.L_timeEnd.TabIndex = 5;
+            this.L_timeEnd.Text = "记录表结束时间";
+            // 
+            // B_Confirm
+            // 
+            this.B_Confirm.Location = new System.Drawing.Point(174, 457);
+            this.B_Confirm.Name = "B_Confirm";
+            this.B_Confirm.Size = new System.Drawing.Size(75, 23);
+            this.B_Confirm.TabIndex = 6;
+            this.B_Confirm.Text = "导出";
+            this.B_Confirm.UseVisualStyleBackColor = true;
+            this.B_Confirm.Click += new System.EventHandler(this.B_Confirm_Click);
+            // 
+            // B_Exit
+            // 
+            this.B_Exit.Location = new System.Drawing.Point(290, 457);
+            this.B_Exit.Name = "B_Exit";
+            this.B_Exit.Size = new System.Drawing.Size(75, 23);
+            this.B_Exit.TabIndex = 7;
+            this.B_Exit.Text = "退出";
+            this.B_Exit.UseVisualStyleBackColor = true;
+            this.B_Exit.Click += new System.EventHandler(this.B_Exit_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 214);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(396, 222);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.TabStop = false;
+            // 
+            // B_ViewData
+            // 
+            this.B_ViewData.Location = new System.Drawing.Point(53, 165);
+            this.B_ViewData.Name = "B_ViewData";
+            this.B_ViewData.Size = new System.Drawing.Size(327, 23);
+            this.B_ViewData.TabIndex = 9;
+            this.B_ViewData.Text = "预览数据";
+            this.B_ViewData.UseVisualStyleBackColor = true;
+            this.B_ViewData.Click += new System.EventHandler(this.B_ViewData_Click);
+            // 
+            // B_DeletSql
+            // 
+            this.B_DeletSql.Location = new System.Drawing.Point(53, 457);
+            this.B_DeletSql.Name = "B_DeletSql";
+            this.B_DeletSql.Size = new System.Drawing.Size(75, 23);
+            this.B_DeletSql.TabIndex = 10;
+            this.B_DeletSql.Text = "收缩";
+            this.B_DeletSql.UseVisualStyleBackColor = true;
+            this.B_DeletSql.Click += new System.EventHandler(this.B_DeletSql_Click);
+            // 
+            // TB_equipName
+            // 
+            this.TB_equipName.Location = new System.Drawing.Point(165, 53);
+            this.TB_equipName.Name = "TB_equipName";
+            this.TB_equipName.Size = new System.Drawing.Size(100, 21);
+            this.TB_equipName.TabIndex = 11;
+            // 
+            // L_equipName
+            // 
+            this.L_equipName.AutoSize = true;
+            this.L_equipName.Location = new System.Drawing.Point(101, 62);
+            this.L_equipName.Name = "L_equipName";
+            this.L_equipName.Size = new System.Drawing.Size(53, 12);
+            this.L_equipName.TabIndex = 12;
+            this.L_equipName.Text = "设备名称";
+            // 
+            // Win_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 509);
+            this.Controls.Add(this.L_equipName);
+            this.Controls.Add(this.TB_equipName);
+            this.Controls.Add(this.B_DeletSql);
+            this.Controls.Add(this.B_ViewData);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.B_Exit);
+            this.Controls.Add(this.B_Confirm);
+            this.Controls.Add(this.L_timeEnd);
+            this.Controls.Add(this.L_timeBegin);
+            this.Controls.Add(this.DTP_endTime);
+            this.Controls.Add(this.DTP_beginTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CB_ChooseTab);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Win_Main";
+            this.Text = "报表管理";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox CB_ChooseTab;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker DTP_beginTime;
+        private System.Windows.Forms.DateTimePicker DTP_endTime;
+        private System.Windows.Forms.Label L_timeBegin;
+        private System.Windows.Forms.Label L_timeEnd;
+        private System.Windows.Forms.Button B_Confirm;
+        private System.Windows.Forms.Button B_Exit;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button B_ViewData;
+        private System.Windows.Forms.Button B_DeletSql;
+        private System.Windows.Forms.TextBox TB_equipName;
+        private System.Windows.Forms.Label L_equipName;
+    }
+}
+
---
+++ TableManager/TableManager/Form1.cs
@@ -0,0 +1,409 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Reflection;
+using Microsoft.Office.Core;
+using System.IO;
+using Microsoft.Office.Interop;
+using System.Runtime.InteropServices;
+
+
+
+namespace TableManager
+{
+    public partial class Win_Main : Form
+    {
+        public string table;//记录用户所选报表
+        public string logtimeName;//报表中的时间名称
+        public string equipName;
+
+        public string Get_Config()
+        {
+            StreamReader sr = new StreamReader(System.IO.Directory.GetCurrentDirectory()+"/config.txt");
+            string line;
+            line = sr.ReadLine();
+            //构建数据库连接字符串
+            string source = "server=(local);" +
+                     "integrated security=SSPI;" +
+                     "database="+line;
+            sr.Close();
+            return source;
+        }
+
+        public Win_Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+       {
+           CB_ChooseTab.Text = CB_ChooseTab.Items[0].ToString();
+        }
+
+        private void Choose_mod_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string[] items=new string[6];//combobox选项
+            string[] tablesName = new string[6];//对应选项报表在数据库中的名称
+            string[] tablesTimeName = new string[6];//对应选项报表时间列名称
+            items[0] = "模式接收记录";
+            items[1] = "模式运行记录";
+            items[2] = "报警记录";
+            items[3] = "模拟量历史记录";
+            items[4] = "用户操作记录";
+            items[5] = "设备动作记录";
+            //items[5] = "参数设置记录";
+
+            tablesName[0] = "modereceive";
+            tablesName[1] = "moderun";
+            tablesName[2] = "ALARMS";
+            tablesName[3] = "HJCS";
+            tablesName[4] = "OPERLOG";
+            tablesName[5] = "equipmentrun";
+            //tablesName[5] = "parasetclass";
+
+            tablesTimeName[0] = "LOGTIME";
+            tablesTimeName[1] = "LOGTIME";
+            tablesTimeName[2] = "itime";
+            tablesTimeName[3] = "TIMESTAMP";
+            tablesTimeName[4] = "TMSTAMP";
+            tablesTimeName[5] = "LOGTIME";
+            //tablesTimeName[5] = "";
+ 
+            Dictionary<string, string> tablePro = new Dictionary<string, string>();
+            tablePro.Add(tablesName[0], tablesTimeName[0]);
+            tablePro.Add(tablesName[1], tablesTimeName[1]);
+            tablePro.Add(tablesName[2], tablesTimeName[2]);
+            tablePro.Add(tablesName[3], tablesTimeName[3]);
+            tablePro.Add(tablesName[4], tablesTimeName[4]);
+            tablePro.Add(tablesName[5], tablesTimeName[5]);
+
+
+            
+            //用户选择不同报表
+            //模式接收记录modereceive
+            if (CB_ChooseTab.Text == items[0])
+            {
+                table = tablesName[0];
+                logtimeName = tablePro[tablesName[0]];
+                TB_equipName.Enabled = false;
+            }
+            //模式运行记录moderun
+            if (CB_ChooseTab.Text == items[1])
+            {
+                table = tablesName[1];
+                logtimeName = tablePro[tablesName[1]];
+                TB_equipName.Enabled = false;
+            }
+            //报警记录ALARMS
+            if (CB_ChooseTab.Text == items[2])
+            {
+                table = tablesName[2];
+                logtimeName = tablePro[tablesName[2]];
+                TB_equipName.Enabled = false;
+                //equipName = "name";
+            }
+            //模拟量历史记录HJCS
+            if (CB_ChooseTab.Text == items[3])
+            {
+                table = tablesName[3];
+                logtimeName = tablePro[tablesName[3]];
+                TB_equipName.Enabled = true;
+                //equipName = "devicename";
+                //DTP_beginTime.Enabled = false;
+                //DTP_endTime.Enabled = false;
+
+            }
+            //用户操作记录OPERLOG
+            if (CB_ChooseTab.Text == items[4])
+            {
+                table = tablesName[4];
+                logtimeName = tablePro[tablesName[4]];
+                TB_equipName.Enabled = false;
+            }
+            //设备运行记录equipmentrun
+            if (CB_ChooseTab.Text == items[5])
+            {
+                table = tablesName[5];
+                logtimeName = tablePro[tablesName[5]];
+                TB_equipName.Enabled = true;
+            }
+ 
+        }
+
+        public void printAll(DataTable dt)
+        {
+            if (dt != null)
+            {
+                try
+                {
+                    //保存文件
+                    SaveFileDialog saveFileDialog = new SaveFileDialog();
+                    saveFileDialog.Filter = "导出Excel (*.xls)|*.xls";
+                    saveFileDialog.FilterIndex = 0;
+                    saveFileDialog.RestoreDirectory = true;
+                    saveFileDialog.CreatePrompt = false;
+                    saveFileDialog.Title = "导出文件保存路径";
+                    saveFileDialog.ShowDialog();
+                    string strName = saveFileDialog.FileName;
+                    if (strName.Length != 0)
+                    {
+                        System.Reflection.Missing miss = System.Reflection.Missing.Value;
+                        //实例化一个excel对象
+                        Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+                        excel.Application.Workbooks.Add(true);
+                        excel.Visible = false;
+                        if (excel == null)
+                        {
+                            MessageBox.Show("Excel无法启动!", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
+
+                        Microsoft.Office.Interop.Excel.Workbooks books = (Microsoft.Office.Interop.Excel.Workbooks)excel.Workbooks;
+                        Microsoft.Office.Interop.Excel.Workbook book = (Microsoft.Office.Interop.Excel.Workbook)(books.Add(miss));
+                        Microsoft.Office.Interop.Excel.Worksheet sheet = (Microsoft.Office.Interop.Excel.Worksheet)book.ActiveSheet;
+                        sheet.Name = "sheet1";
+                        //int m = 0, n = 0;
+
+                        //生成表头
+                        for (int i = 0; i < dt.Columns.Count; i++)
+                        {
+                            excel.Cells[1, i + 1] = dataGridView1.Columns[i].HeaderText;
+                        }
+                        //写入数据
+                        if (dt.Rows.Count > 0)
+                        {
+                            for (int i = 0; i < dt.Rows.Count; i++)
+                            {
+                                for (int j = 0; j < dt.Columns.Count; j++)
+                                {
+                                    string str = dt.Rows[i][j].ToString();
+                                    excel.Cells[i + 2, j + 1] = str;
+                                }
+                            }
+
+                        }
+                        sheet.SaveAs(strName, miss, miss, miss, miss, miss, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange, miss, miss, miss);
+                        book.Close(false, miss, miss);
+                        books.Close();
+                        excel.Quit();
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(sheet);
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(book);
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(books);
+                        System.Runtime.InteropServices.Marshal.ReleaseComObject(excel);
+
+                        GC.Collect();
+                        MessageBox.Show("数据已成功导出!", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        System.Diagnostics.Process.Start(strName);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "错误提示");
+                }
+
+            }
+            
+        }
+
+        private DataTable GetDataTableFromSqlServer()
+        {
+            DateTime beginTime;// 开始时间
+            DateTime endTime;//结束时间
+            string select;
+
+            beginTime = DTP_beginTime.Value;
+            endTime = DTP_endTime.Value;
+            equipName = TB_equipName.Text;
+            //构建sql筛选语句
+            if (CB_ChooseTab.Text == "设备动作记录")
+            {
+                if (TB_equipName.Text == "")
+                {
+                    select = "SELECT * FROM " +
+                table +
+                " WHERE " +
+                logtimeName + " BETWEEN" +
+                "'" + beginTime.ToString() + "'" +
+                "AND" +
+                "'" + endTime.ToString() + "'";
+                }
+                else
+                {
+                    select = "SELECT * FROM " +
+                table +
+                " WHERE TAGNAME LIKE " +
+                "'%" + equipName + "%'" +
+                " AND " +
+                logtimeName + " BETWEEN" +
+                "'" + beginTime.ToString() + "'" +
+                "AND" +
+                "'" + endTime.ToString() + "'";
+                }
+
+     
+            }
+            if (CB_ChooseTab.Text == "模拟量历史记录")
+            {
+                if (TB_equipName.Text == "")
+                {
+                    select = "SELECT * FROM " +
+                table +
+                " WHERE " +
+                logtimeName + " BETWEEN" +
+                "'" + beginTime.ToString() + "'" +
+                "AND" +
+                "'" + endTime.ToString() + "'";
+                }
+                else
+                {
+                    
+                    string select1 = "SELECT NAME FROM syscolumns where id=object_id('HJCS')";//获取全部列名
+                    //创建一个列名为内容的DataTable
+                    SqlConnection newConn = new SqlConnection(Get_Config());
+                    newConn.Open();
+                    SqlCommand getColumnCmd = new SqlCommand(select1, newConn);
+                    SqlDataAdapter getColumnDa = new SqlDataAdapter(getColumnCmd);
+                    DataTable columnsDt = new DataTable();
+                    getColumnDa.Fill(columnsDt);
+                    newConn.Close();
+
+
+
+                    
+                    DataRow[] drs = columnsDt.Select("name like " + "'" + equipName + "%'");//筛选
+
+                    //将筛选出来的内容转化为string
+                    string[] sendto = new string[drs.Length];
+                    string columnName = "";
+                    int drRow=0;
+                    foreach (DataRow dra in drs)
+                    {
+                        sendto[drRow] = dra["name"].ToString();
+                        drRow++;
+                    }
+                    
+                    for (int i = 0; i < sendto.Length; i++)
+                    {
+                        columnName =columnName+ "," + sendto[i] ;
+                    }
+
+                    //构造用于操作数据库的命令字符串
+                    select = "SELECT " + "FLTIME,TIMESTAMP" + columnName + " FROM HJCS"
+                             +
+                             " WHERE " +
+                             logtimeName + " BETWEEN" +
+                             "'" + beginTime.ToString() + "'" +
+                             "AND" +
+                             "'" + endTime.ToString() + "'";
+ 
+                }
+
+                
+
+                
+                
+
+            }
+            else
+            {
+                select = "SELECT * FROM " +
+                table +
+                " WHERE " +
+                logtimeName + " BETWEEN" +
+                "'" + beginTime.ToString() + "'" +
+                "AND" +
+                "'" + endTime.ToString() + "'";
+            }
+            
+            try
+            {
+                SqlConnection conn = new SqlConnection(Get_Config());
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(select, conn);
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                conn.Close();
+                return dt;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message, "错误提示");
+                return null; 
+            }
+        }
+
+
+
+
+        private void B_Confirm_Click(object sender, EventArgs e)
+        {
+            if (DTP_beginTime.Value > DTP_endTime.Value)
+            {
+                MessageBox.Show("起始日期大于结束日期!", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                printAll(GetDataTableFromSqlServer());
+            }
+            //printAll(GetDataTableFromSqlServer());
+
+        }
+                    
+
+        private void B_Exit_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(0);
+        }
+
+        private void B_ViewData_Click(object sender, EventArgs e)
+        {
+            if (DTP_beginTime.Value > DTP_endTime.Value)
+            {
+                MessageBox.Show("起始日期大于结束日期!", "警告", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                dataGridView1.DataSource = GetDataTableFromSqlServer();
+            }
+            //dataGridView1.DataSource = GetDataTableFromSqlServer();
+
+        }
+
+        private void B_DeletSql_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("确定收缩所选报表吗?", "询问", MessageBoxButtons.YesNo) == DialogResult.Yes)
+            {
+                try
+                {
+                    string delete = "DELETE FROM " +
+                                    table +
+                                    " WHERE " + logtimeName + " <" +
+                                    "'" + DateTime.Now.AddDays(-180).ToString() + "'";
+                    SqlConnection conn = new SqlConnection(Get_Config());
+                    conn.Open();
+                    SqlCommand cmd = new SqlCommand(delete, conn); 
+                    int i=cmd.ExecuteNonQuery();
+                    MessageBox.Show("成功删除" + i + "行数据", "提示", MessageBoxButtons.OK);
+                    conn.Close();
+                }
+                catch(Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "错误提示");
+                }
+            }
+            
+
+        }
+
+     
+
+    }
+}
